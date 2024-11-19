d2a2f3477a5ff6823d6de4f07fc3f452067f1cf4
Eugene Dobry <eugene@dobry.me>
2024-11-19 17:29:56 -0500

    add gcloud snapshot helpers

--- chains/gcloud/gcloud.sh
+++ chains/gcloud/gcloud.sh
@@ -1,3 +1,17 @@
 function gcloudConfigPath() {
     gcloud info --format="get(config.paths.active_config_path)"
 }
+
+function gcloudListSnapshots() {
+    gcloud compute snapshots list --format="get(name)"
+}
+
+function gcloudGetSnapshots() {
+    gcloud compute snapshots list --format=json 2>/dev/null | jq -c
+}
+
+function gcloudGetSnapshot() {
+    requireArg "a snapshot name" "$1" || return 1
+
+    gcloud compute snapshots list --filter="name=($1)" --format=json | jq -c
+}
