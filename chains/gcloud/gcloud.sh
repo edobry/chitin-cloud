20c6bb47a5f4923ca5c1f9edb698e2a97c6164b0
Eugene Dobry <eugene@dobry.me>
2024-12-03 19:06:13 -0500

    add workflow helpers

--- chains/gcloud/gcloud.sh
+++ chains/gcloud/gcloud.sh
@@ -21,3 +21,41 @@ function gcloudGetSnapshot() {
 
     gcloud compute snapshots list --filter="name=($1)" --format=json | jq -c
 }
+
+function gcloudShowWorkflows() {
+    gcloud workflows list $*
+}
+
+function gcloudGetWorkflows() {
+    gcloudShowWorkflows --format=json | jq
+}
+
+function gcloudListWorkflows() {
+    gcloudShowWorkflows --format="get(name)" | cut -d '/' -f6
+}
+
+function gcloudListWorkflows() {
+    gcloudShowWorkflows --format="get(name)" | cut -d '/' -f6
+}
+
+function gcloudGetWorkflow() {
+    requireArg "a workflow name" "$1" || return 1
+
+    gcloud workflows describe "$1" --format=json 2>/dev/null | jq -c
+}
+
+function gcloudListWorkflowSteps() {
+    requireArg "a workflow name" "$1" || return 1
+
+    gcloudGetWorkflow "$1" | jq -r '.sourceContents' \
+        | sed -E 's/(^|[^"])((\$\{[^}]+\}))($|[^"])/\1'''\2'''\4/g' | yq -o=json \
+        | jq -r '[.[] | to_entries] | add[] | "\(.key)"'
+}
+
+function gcloudRunWorkflow() {
+    requireArg "a workflow name" "$1" || return 1
+
+    local workflowName="$1"; shift
+
+    gcloud workflows run "$workflowName" --call-log-level=log-all-calls $*
+}
