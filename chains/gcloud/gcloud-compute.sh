dbcd365fbebabf27b688e00a111bff5da4cb915e
Eugene Dobry <eugene@dobry.me>
2025-02-14 19:08:54 -0500

    add gcloudCreateSnapshot

--- chains/gcloud/gcloud-compute.sh
+++ chains/gcloud/gcloud-compute.sh
@@ -5,3 +5,24 @@ function gcloudComputeGetRegions() {
 function gcloudComputeListRegions() {
     gcloudComputeGetRegions | jq -r '.name'
 }
+
+function gcloudListSnapshots() {
+    gcloud compute snapshots list --format="get(name)"
+}
+
+function gcloudGetSnapshots() {
+    gcloud compute snapshots list --format=json 2>/dev/null | jq -c
+}
+
+function gcloudGetSnapshot() {
+    requireArg "a snapshot name" "$1" || return 1
+
+    gcloud compute snapshots list --filter="name=($1)" --format=json | jq -c
+}
+
+function gcloudCreateSnapshot() {
+    requireArg "a disk name" "$1" || return 1
+    requireArg "a snapshot name" "$2" || return 1
+
+    gcloud compute snapshots create "$2" --source-disk="$2"
+}
--- chains/gcloud/gcloud.sh
+++ chains/gcloud/gcloud.sh
@@ -8,20 +8,6 @@ function gcloudDockerConfigure() {
     gcloud auth configure-docker
 }
 
-function gcloudListSnapshots() {
-    gcloud compute snapshots list --format="get(name)"
-}
-
-function gcloudGetSnapshots() {
-    gcloud compute snapshots list --format=json 2>/dev/null | jq -c
-}
-
-function gcloudGetSnapshot() {
-    requireArg "a snapshot name" "$1" || return 1
-
-    gcloud compute snapshots list --filter="name=($1)" --format=json | jq -c
-}
-
 function gcloudShowWorkflows() {
     gcloud workflows list $*
 }
